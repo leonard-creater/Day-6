6fed424dd2b9052e70984a931c28deed88ee6d05
leonard-creater <smileme12369@gmail.com>
2024-11-05 11:26:44 +0000

    shapes

---
+++ 11/5/2024/shapes.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.Screen().bgcolor("lightblue")
+sc=turtle.Screen()
+sc.setup(450.500)
+board=turtle.Turtle()
+
+for i in range (4):
+    board.forward(260)
+    board.right(90)
+    i=i+1
+
+turtle.done()
